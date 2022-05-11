3f4cb8668bb58fcca4ebcd52eebd4b9aaf4924dd
Arthur Grossi Fernandes <arthur17000@gmail.com>
2022-05-11 15:33:16 -0300

    Edit 1

--- modulo1/strings-arrays/template_javascript/index.js
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -22,10 +22,10 @@ console.log ("Essas são as minhas comidas preferidas:\n",comidasArray[0] ,"\n",
 
 //Exercício 3
 
-const listaDeTarefas =[]
-listaDeTarefas.push (prompt("Adicione a tarefa número 0!"), prompt("Adicione a tarefa número 1!"), prompt("Adicione a tarefa número 2!"))
+const listaDeTarefas = []
+listaDeTarefas.push (prompt("Adicione a tarefa número 1!"), prompt("Adicione a tarefa número 2!"), prompt("Adicione a tarefa número 3!"))
 console.log (listaDeTarefas)
-listaDeTarefas.splice (Number(prompt("Qual tarefa você completou de 0 a 2?")), 1)
+listaDeTarefas.splice (Number(prompt("Qual tarefa você completou de 1 a 3?"))-1, 1)
 console.log (listaDeTarefas)
 
 //Desafio
