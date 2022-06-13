a376fd27947fdf2b554ef648e282ef6c51998686
Arthur Grossi Fernandes <arthur17000@gmail.com>
2022-06-13 15:22:45 -0300

    Atividade Rend Condicional

---
+++ modulo2/rend-condicional-form/src/pages/Etapa2.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Etapa2 extends React.Component{
+
+    render(){
+        return (
+            <>
+             <h1>Etapa 2: Acadêmico </h1>
+             <p>1. Qual o Curso?</p>
+             <input/>
+             <p>2. Qual a Unidade de Ensino?</p>
+             <input/>
+            </>
+        );
+    }
+}
---
+++ modulo2/rend-condicional-form/src/pages/EtapaFinal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class EtapaFinal extends React.Component{
+
+    render(){
+        return (
+            <>
+             <h1>Obrigado por participar!</h1>
+            </>
+        );
+    }
+}
